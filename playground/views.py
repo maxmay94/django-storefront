48fa7f21f3e6c8c323b146cce909c7d9e955397f
maxmay94 <maxmay94@gmail.com>
2023-04-10 19:16:22 -0400

    working with expression wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count
+from django.db.models import Value, F, Func, Count, ExpressionWrapper
 from django.db.models.functions import Concat
 from store.models import Product, Order, Customer
 
@@ -15,8 +15,15 @@ def say_hello(request):
   # queryset = Customer.objects.annotate(
   #   full_name = Concat('first_name', Value(' '), 'last_name')
   # )
-  queryset = Customer.objects.annotate(
-    orders_count=Count('order')
+  # queryset = Customer.objects.annotate(
+  #   orders_count=Count('order')
+  # )
+
+  discounted_price = ExpressionWrapper(
+    F('unit_price') * 0.8, output_field=DecimalField())
+
+  queryset = Product.objects.annotate(
+    discounted_price=discounted_price
   )
 
   return render(request, 'hello.html', {'name': 'Maximillian', 'result': list(queryset)})
