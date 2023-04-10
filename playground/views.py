bde8280c8edac1f3a2d6b33ed74dd5a217e2e9c7
maxmay94 <maxmay94@gmail.com>
2023-04-10 18:59:25 -0400

    annotating objects

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.db.models.aggregates import Count, Max, Min, Sum, Avg
-from store.models import Product, Order
+from django.db.models import Value, F
+from store.models import Product, Order, Customer
 
 # Create your views here.
 # request --> HttpResponse
@@ -8,7 +8,6 @@ from store.models import Product, Order
 # action
 
 def say_hello(request):
-  # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
-  result = Product.objects.filter(collection_id=3).aggregate(count = Count('id'),min_price=Min('unit_price'), max_price=Max('unit_price'), avg_price=Avg('unit_price'))
+  queryset = Customer.objects.annotate(new_id=F('id') + 1)
 
-  return render(request, 'hello.html', {'name': 'Maximillian', 'result': result})
+  return render(request, 'hello.html', {'name': 'Maximillian', 'result': list(queryset)})
