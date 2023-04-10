218d94e1bd4579416ac6f598b56bce92c7235dcd
maxmay94 <maxmay94@gmail.com>
2023-04-10 19:26:06 -0400

    quereying generic relationships

--- playground/views.py
+++ playground/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count, ExpressionWrapper
-from django.db.models.functions import Concat
-from store.models import Product, Order, Customer
-
-# Create your views here.
-# request --> HttpResponse
-# request handler 
-# action
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 def say_hello(request):
-  # queryset = Customer.objects.annotate(
-  #   full_name = Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
-  # )
-  # queryset = Customer.objects.annotate(
-  #   full_name = Concat('first_name', Value(' '), 'last_name')
-  # )
-  # queryset = Customer.objects.annotate(
-  #   orders_count=Count('order')
-  # )
+  conent_type = ContentType.objects.get_for_model(Product)
 
-  discounted_price = ExpressionWrapper(
-    F('unit_price') * 0.8, output_field=DecimalField())
+  queryset = TaggedItem.objects \
+    .select_related('tag') \
+    .filter(
+      content_type=conent_type,
+      object_id=1
+    )
 
-  queryset = Product.objects.annotate(
-    discounted_price=discounted_price
-  )
-
-  return render(request, 'hello.html', {'name': 'Maximillian', 'result': list(queryset)})
+  return render(request, 'hello.html', {'name': 'Maximillian', 'tags': list(queryset)})
