3b04ae1da2980bb7a41c2a5bdf78c08e91602e27
maxmay94 <maxmay94@gmail.com>
2023-04-10 18:53:54 -0400

    aggregating objects

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Count, Max, Min, Sum, Avg
 from store.models import Product, Order
 
 # Create your views here.
@@ -8,6 +9,6 @@ from store.models import Product, Order
 
 def say_hello(request):
   # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+  result = Product.objects.filter(collection_id=3).aggregate(count = Count('id'),min_price=Min('unit_price'), max_price=Max('unit_price'), avg_price=Avg('unit_price'))
 
-  queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-  return render(request, 'hello.html', {'name': 'Maximillian', 'orders' : list(queryset)})
+  return render(request, 'hello.html', {'name': 'Maximillian', 'result': result})
