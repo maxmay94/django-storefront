6d5a9f0a64d00ac803c71e0b9ce378b59908a6ac
maxmay94 <maxmay94@gmail.com>
2023-04-11 10:27:34 -0400

    caching

--- playground/views.py
+++ playground/views.py
@@ -4,13 +4,9 @@ from store.models import Product
 from tags.models import TaggedItem
 
 def say_hello(request):
-  conent_type = ContentType.objects.get_for_model(Product)
 
-  queryset = TaggedItem.objects \
-    .select_related('tag') \
-    .filter(
-      content_type=conent_type,
-      object_id=1
-    )
+  queryset = Product.objects.all()
+  list(queryset)
+  queryset[0]
 
-  return render(request, 'hello.html', {'name': 'Maximillian', 'tags': list(queryset)})
+  return render(request, 'hello.html', {'name': 'Maximillian'})
