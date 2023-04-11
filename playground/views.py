0de9c6b1c8c1ec172022e57bde05718bd4f6e559
maxmay94 <maxmay94@gmail.com>
2023-04-11 11:09:26 -0400

    executing raw sql

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import transaction, connection
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Order, OrderItem, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
+  with connection.cursor() as cursor:
+    cursor.callproc('get_customers', [1,2,'a'])
 
-  with transaction.atomic():
-    order = Order()
-    order.customer_id = 1
-    order.save()
-
-    item = OrderItem()
-    item.order = order
-    item.product_id = -1
-    item.quantity = 1
-    item.unit_price = 10
-    item.save()
-
-  return render(request, 'hello.html', {'name': 'Maximillian'})
+  return render(request, 'hello.html', {'name': 'Maximillian', 'result': list(queryset)})
