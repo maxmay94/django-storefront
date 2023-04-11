d3df1282f5e565123205351407a67e1aeaf8161d
maxmay94 <maxmay94@gmail.com>
2023-04-11 11:05:02 -0400

    transactions

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Order, OrderItem, Collection
 from tags.models import TaggedItem
 
 def say_hello(request):
 
-  collection = Collection.objects.get(pk=11)
-  collection.featured_product = None
-  collection.save()
+  with transaction.atomic():
+    order = Order()
+    order.customer_id = 1
+    order.save()
 
-  Collection.objects.filter(pk=11).update(featured_product=None)
+    item = OrderItem()
+    item.order = order
+    item.product_id = -1
+    item.quantity = 1
+    item.unit_price = 10
+    item.save()
 
   return render(request, 'hello.html', {'name': 'Maximillian'})
